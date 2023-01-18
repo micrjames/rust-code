9951de2821066e233c95bb37d7cbeb56025cc887
Michael James <micrjames921@gmail.com>
2023-01-17 21:08:11 -0700

    learning more about ownership here

--- own/src/main.rs
+++ own/src/main.rs
@@ -1,17 +1,15 @@
 fn main() {
-    let s: String = String::from("hello");
-    takes_ownership(some_string: s);
-
-    println!("{}", s);
-
-    let x: i32 = 5;
-    makes_copy(some_integer: x);
-    println!("{}", x);
+    let s1: String = gives_ownership();
+    let s2: String = String::from("hello");
+    let s3: String = takes_and_gives_back(s2);
+    println!("s1 = {}, s3 = {}", s1, s3);
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+fn gives_ownership() -> String {
+    let some_string: String = String::from("hello");
+    some_string     // Moves ownership to s1
 }
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
